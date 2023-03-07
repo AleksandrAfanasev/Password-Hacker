88fe7ba3548f36cf6042354921bf12747621220b
AleksandrAfanasev <jgBeM9bkX@@hEq3>
2023-03-07 19:48:22 +0300

    Stage 2/5: Simple brute force

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,21 +1,27 @@
 import socket
 import sys
+import itertools
 
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
 args = sys.argv
 # working with a socket as a context manager
 with socket.socket() as client_socket:
     hostname = args[1]
     port = int(args[2])
     address = (hostname, port)
-
     client_socket.connect(address)
+    while True:
+        for i in range(1000000):
+            for item in itertools.product(alphabet, repeat=i + 1):
+                password = ''.join(item).encode()
 
-    data = args[3].encode()
+                client_socket.send(password)
 
-    client_socket.send(data)
+                response = client_socket.recv(1024)
 
-    response = client_socket.recv(1024)
-
-    response = response.decode()
-    print(response)
+                if response.decode() == 'Connection success!':
+                    print(password.decode())
+                    exit()
+                else:
+                    continue
